c38753b0101e557dff6a1655b136286c89b678d7
thexuanngu <89938666+thexuanngu@users.noreply.github.com>
2026-08-29 02:19:09 +0100

    Add: plus-one - submission-7

---
+++ Data Structures & Algorithms/plus-one/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+                if (i == 0) {
+                    vector<int> result(digits.size() + 1, 0);
+                    result[0] = 1;
+                    return result;
+                }
+            }
+        }
+    }
+};
